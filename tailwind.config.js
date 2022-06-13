0a0314cca0004770af2d15e8823baa197bae79c3
Prakash Shrestha <86401764+prakash-2021@users.noreply.github.com>
2022-06-13 12:09:57 +0545

    changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         linkCOlor: "#1c3f3a",
         midGray: "#ece8d9",
         h1Color: "#102226",
-        btnColor: "#26534c",
+        darkGreen: "#26534c",
         btnHover: "#1b3b37",
         btnHover2: "#e0eae8",
         greatColor: "#cbc7b5",
